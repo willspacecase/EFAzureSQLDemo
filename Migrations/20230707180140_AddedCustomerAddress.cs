c473aa2656d8118dcf1616995024b85c5cd09b41
NORTHAMERICA\williamcase <williamcase@microsoft.com>
2023-07-11 15:56:34 -0400

    Added customer address field

---
+++ Migrations/20230707180140_AddedCustomerAddress.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFAzureSQLDemo.Migrations
+{
+    public partial class AddedCustomerAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
